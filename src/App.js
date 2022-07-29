32c6b93007a2f6761c63b0a23a679232b5ef9599
Philip Clark <109166407+PhilipClark-SpamAccount@users.noreply.github.com>
2022-07-29 16:47:15 -0400

    Updated 192 icon

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Aos from 'aos';
 import React, { useEffect } from 'react';
-import createScrollSnap from 'scroll-snap';
 import { ThemeProvider } from 'styled-components';
 import './App.css';
 import About from './components/About';
@@ -14,20 +13,6 @@ import { theme } from './components/styles/theme';
 import { logFirebaseEvent } from './firebase/config';
 
 function App() {
-  const element = document.getElementById('root');
-  const { bind, unbind } = createScrollSnap(
-    element,
-    {
-      snapDestinationX: '0%',
-      snapDestinationY: '90%',
-      timeout: 100,
-      duration: 300,
-      threshold: 0.5,
-      snapStop: false,
-    },
-    () => console.log('element snapped')
-  );
-
   useEffect(() => {
     Aos.init({});
     logFirebaseEvent('Site Loaded');
